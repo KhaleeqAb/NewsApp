21a691a0d76a72b78f1a3070bcfbc1fb2bb6f99d
Khaleeq <abdulkhaleeq66@gmail.com>
2017-06-19 12:50:57 -0400

    Done With StoryBoard ViewControllers and Classes.

---
+++ NewsApp/NewsApp/FavoritesDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesDetailVC.h
+//  NewsApp
+//
+//  Created by rtuser on 6/19/17.
+//  Copyright © 2017 rt_user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesDetailVC : UIPageViewController
+
+@end
